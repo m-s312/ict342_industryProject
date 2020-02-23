f25bade810461c8709aae5c3b00e6687cbe929dc
VenkataSambasivaRao <sambakesanam@gmail.com>
2020-02-23 10:55:02 +0400

    Add project files.

---
+++ MarketingServiceRequests/Controllers/ServicedetailsController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using MarketingServiceRequests.Models;
+
+namespace MarketingServiceRequests.Controllers
+{
+    public class ServicedetailsController : Controller
+    {
+        // GET: Servicedetails
+        [HttpGet]
+        public ActionResult servicedetail()
+        {
+            //if (Session["Id"] == null)
+            //    return RedirectToAction("Index", "User");
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult servicedetail(ServiceDetails paramservicedetails, ServiceDetailsSelection paramcheckboxes)
+        {
+            if (ModelState != null && ModelState.IsValid)
+            {
+                Utility objUtility = new Utility();
+                Session["Id"] = objUtility.CreateServiceDetail(paramservicedetails, paramcheckboxes, Session["ContactId"].ToString());
+                return RedirectToAction("servicecategories", "Servicedetails");
+            }            
+            return View();
+        }
+
+        [HttpGet]
+        [ActionName("servicecategories")]
+        public ActionResult servicecategories()
+        {
+            if (Session["Id"] == null)
+                return RedirectToAction("servicedetail", "Servicedetails");
+            //return RedirectToAction("Index", "User");
+
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("servicecategories")]
+        public ActionResult servicecategories_post()
+        {
+            if (ModelState != null && ModelState.IsValid) {
+                return RedirectToAction("Agreement", "ServiceAgreement");
+            }
+            return View();
+        }
+
+
+
+
+
+    }
+}
